dccef42404e17a194131675c75bddb5aea913ac3
Anramilt <hiroko.anramilt@gmail.com>
2025-02-24 16:18:15 +0700

    Hello world!

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*const (
+	ExpectedUsername = "user"
+	ExpectedPassword = "secret"
+)*/
+
+func main() {
+	//ConnectDB()
+
+	fmt.Println("Hello world!")
+
+	//logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+
+	//регистрирует нашу функцию обработчика  для обработки всех запросов GET
+	//http.HandleFunc("/", handler)
+	//http.HandleFunc("/login", authHandler)              //обработчик авторизации
+	//http.HandleFunc("/registration", addAccountHandler) //добавление аккаунта в БД
+	//http.HandleFunc("/echo", auth(echoHandler))         //предоставление доступа (к эхо) по авторизации
+	//http.HandleFunc("/addecho", auth(addEchoHandler))   //добавить БД в эхо
+
+}
